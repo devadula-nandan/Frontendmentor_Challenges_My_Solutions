13e514f1728c80e68c49aa944ab9af8b7d57111c
nandan <devadula.nandan@gmail.com>
2022-09-06 13:22:42 +0530

    relatice text on cards

--- src/route-pages/challenge_2/Challenge_2.jsx
+++ src/route-pages/challenge_2/Challenge_2.jsx
@@ -2,11 +2,21 @@ import cf from './images/bg-card-front.png'
 import cb from './images/bg-card-back.png'
 export const Challenge_2 = () => {
     return (
-        <div className="w-screen h-screen">
-            <div className={`w-full lg:w-1/3 lg:h-full h-1/3 qoHsFYAt bg-cover bg-center flex justify-center items-center flex-col`}>
-                <img src={cf} alt="" className=' shadow-xl rounded-xl absolute lg:static lg:mb-[calc(2rem+1vw)] lg:ml-[calc(33.33vw-14rem)] w-3/4 md:w-auto top-[20%] -ml-4' />
-                <img src={cb} alt="" className=' shadow-xl rounded-xl lg:ml-[calc(33vw)] w-3/4 md:w-auto ml-8' />
+        <>
+            <div className="w-screen h-screen">
+                <div className={`w-full lg:w-1/3 lg:h-full qoHsFYAt bg-cover bg-center flex justify-center items-center flex-col`}>
+                    <div className="flex flex-col-reverse lg:flex-col mt-[calc(2rem+1vw+2rem)]">
+                        <div className="relative cf lg:mb-[calc(2rem+1vw)] ml-4 mr-12 lg:ml-0 lg:mr-0 z-10 -mt-[calc(6vw+1rem)] -mb-12">
+                            <img src={cf} alt="" className=' lg:ml-[calc(33vw-18rem)]' />
+                            <div className="cfc absolute top-0 lg:left-[calc(33vw-18rem)]">hi</div>
+                        </div>
+                        <div className="relative ml-12 mr-4 lg:ml-0 lg:mr-0">
+                            <img src={cb} alt="" className=' lg:ml-[calc(33.33vw-14rem)]' />
+                            <div className="cbc absolute top-0 lg:left-[calc(33.33vw-14rem)]">hi</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
