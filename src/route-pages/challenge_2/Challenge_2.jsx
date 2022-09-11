d7d16a30888e7fb1b5866604fb85b07be33c0cc2
nandan <devadula.nandan@gmail.com>
2022-09-11 16:58:37 +0530

    form

--- src/route-pages/challenge_2/Challenge_2.jsx
+++ src/route-pages/challenge_2/Challenge_2.jsx
@@ -40,33 +40,41 @@ export const Challenge_2 = () => {
                     </div>
                 </div>
                 <div className={`w-full lg:w-2/3 lg:h-full bg-cover bg-center flex justify-center items-center flex-col`}>
-                    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 w-full md:w-1/2 lg:px-8 xl:px-20">
                         <div className="w-full max-w-md space-y-8">
                             <form className="mt-8 space-y-6" action="#" method="POST">
                                 <input type="hidden" name="remember" value="true" />
                                 <div className="-space-y-px rounded-md shadow-sm">
                                     <div>
-                                        <label htmlFor="name" className="">Card holder name</label>
-                                        <input id="name" name="name" type="text" autoComplete="name" value={cardInfo.name} required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="e.g. Jane Appleseed" onInput={(e) => { e.target.value.length < 17 && setCardInfo({ ...cardInfo, name: e.target.value }) }} />
+                                        <label htmlFor="name" className=" text-violet-900 font-semibold tracking-widest">CARDHOLDER NAME</label>
+                                        <input id="name" name="name" type="text" autoComplete="name" value={cardInfo.name} required className="mt-2 mb-6 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-md font-semibold" placeholder="e.g. Jane Appleseed" onInput={(e) => { e.target.value.length < 17 && setCardInfo({ ...cardInfo, name: e.target.value }) }} />
                                     </div>
                                     <div>
-                                        <label htmlFor="card-number" className="">Card number</label>
-                                        <input id="card-number" name="card-number" type="number" autoComplete="number" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="e.g. 1234 5678 9123 0000" onWheel={(e) => { e.target.blur() }} value={cardInfo.number} onInput={(e) => { e.target.value.length < 17 && setCardInfo({ ...cardInfo, number: e.target.value }) }} />
+                                        <label htmlFor="card-number" className=" text-violet-900 font-semibold tracking-widest">CARD NUMBER</label>
+                                        <input id="card-number" name="card-number" type="number" autoComplete="number" required className="mt-2 mb-6 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-md font-semibold" placeholder="e.g. 1234 5678 9123 0000" onWheel={(e) => { e.target.blur() }} value={cardInfo.number} onInput={(e) => { e.target.value.length < 17 && setCardInfo({ ...cardInfo, number: e.target.value }) }} />
                                     </div>
-                                    <div>
-                                        <label htmlFor="password" className="">EXP. DATE (MM/YY)</label>
-                                        <input id="card-number" name="card-number" type="number" autoComplete="number" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="MM" onWheel={(e) => { e.target.blur() }} value={cardInfo.expiryM} onInput={(e) => { e.target.value.length < 3 && e.target.value < 13 && setCardInfo({ ...cardInfo, expiryM: e.target.value }) }} />
-                                        <input id="card-number" name="card-number" type="number" autoComplete="number" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="YY" onWheel={(e) => { e.target.blur() }} value={cardInfo.expiryY} onInput={(e) => { e.target.value.length < 3 && setCardInfo({ ...cardInfo, expiryY: e.target.value }) }} />
-                                        <input id="card-number" name="card-number" type="number" autoComplete="number" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="CVV" onWheel={(e) => { e.target.blur() }} value={cardInfo.cvv} onInput={(e) => { e.target.value.length < 4 && setCardInfo({ ...cardInfo, cvv: e.target.value }) }} />
+                                    <div className='flex'>
+                                        <div className="flex flex-col w-1/2 mt-2">
+                                            <label htmlFor="password" className=" text-violet-900 font-semibold tracking-widest">EXP. DATE (MM/YY)</label>
+                                            <div className="flex">
+                                                <input id="card-number" name="card-number" type="number" autoComplete="number" required className="mt-2 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-md font-semibold" placeholder="MM" onWheel={(e) => { e.target.blur() }} value={cardInfo.expiryM} onInput={(e) => { e.target.value.length < 3 && e.target.value < 13 && setCardInfo({ ...cardInfo, expiryM: e.target.value }) }} />
+                                                <input id="card-number" name="card-number" type="number" autoComplete="number" required className="mt-2 ml-2 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-md font-semibold" placeholder="YY" onWheel={(e) => { e.target.blur() }} value={cardInfo.expiryY} onInput={(e) => { e.target.value.length < 3 && setCardInfo({ ...cardInfo, expiryY: e.target.value }) }} />
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col w-1/2 mt-2 ml-4">
+                                            <label htmlFor="password" className=" text-violet-900 font-semibold tracking-widest">EXP. DATE (MM/YY)</label>
+                                            <input id="card-number" name="card-number" type="number" autoComplete="number" required className="mt-2 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-md font-semibold" placeholder="CVV" onWheel={(e) => { e.target.blur() }} value={cardInfo.cvv} onInput={(e) => { e.target.value.length < 4 && setCardInfo({ ...cardInfo, cvv: e.target.value }) }} />
+                                        </div>
+                                        
 
                                     </div>
                                 </div>
 
                                 <div>
-                                    <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                    <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-600 py-2 px-4 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
                                         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
 
-                                            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <svg className="h-5 w-5 text-violet-500 group-hover:text-violet-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
                                             </svg>
                                         </span>
