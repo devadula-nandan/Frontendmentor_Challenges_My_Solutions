6a32b1bf91c3fd4fc9210ad08a4ec098117d3db2
nandan <devadula.nandan@gmail.com>
2022-09-11 17:30:39 +0530

    mb card

--- src/route-pages/welcome.jsx
+++ src/route-pages/welcome.jsx
@@ -8,24 +8,24 @@ export const Welcome = () => {
             <h1 className=" text-center font-semibold text-6xl py-12">Frontend mentor challenges</h1>
             <div className="container mx-auto">
                 <div className="flex flex-wrap justify-center">
-                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4">
+                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4 mb-4">
                         <div className=" rounded-lg overflow-hidden bg-green-300 transition-all cursor-pointer shadow-sm hover:shadow-lg" onClick={() => { navigate("./challenge_1") }}>
                             <img src={Challenge_1} alt="not loaded" className="w-full" />
                             <h1 className="p-3 text-center text-2xl">Password Generator</h1>
                         </div>
                         
                     </div>
-                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4">
+                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4 mb-4">
                         <div className=" rounded-lg overflow-hidden bg-purple-900 transition-all cursor-pointer shadow-sm hover:shadow-lg" onClick={() => { navigate("./challenge_2") }}>
                             <img src={Challenge_2} alt="not loaded" className="w-full" />
                             <h1 className="p-3 text-center text-white text-2xl">Interactive card details form</h1>
                         </div>
                         
                     </div>
-                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4">
+                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4 mb-4">
 
                     </div>
-                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4">
+                    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4 mb-4">
 
                     </div>
 
