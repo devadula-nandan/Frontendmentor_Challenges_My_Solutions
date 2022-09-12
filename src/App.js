eea87eba4258ad205926c602661762d60a9e4bb7
nandan <devadula.nandan@gmail.com>
2022-09-12 21:59:52 +0530

    clock easter egg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Welcome } from './route-pages/welcome';
+import { Clock } from './route-pages/clock';
 import { Challenge_1 } from './route-pages/challenge_1/Challenge_1';
 import { Challenge_2 } from './route-pages/challenge_2/Challenge_2';
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Welcome />} />
           <Route path="/Challenge_1" element={<Challenge_1 />} />
           <Route path="/Challenge_2" element={<Challenge_2 />} />
+          <Route path='/Clock' element={<Clock />} />
         </Routes>
         {/* <AppFooter /> */}
       </Router>
