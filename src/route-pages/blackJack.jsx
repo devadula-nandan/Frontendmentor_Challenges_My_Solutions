03455d8f919c59d3bb16015ce312afc9add0da9f
nandan <devadula.nandan@gmail.com>
2023-03-23 21:52:07 +0530

    using svgs and dynamic button content and logics

--- src/route-pages/blackJack.jsx
+++ src/route-pages/blackJack.jsx
@@ -6,7 +6,7 @@ export const BlackJack = () => {
     const [deck, setDeck] = useState({});
     const [hand, setHand] = useState([]);
     const [score, setScore] = useState(0);
-    const [stand, setStand] = useState(false);
+    const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
         let config = {
@@ -24,19 +24,21 @@ export const BlackJack = () => {
     }, [])
 
     function hit(count, deck_id) {
-        if (stand) {
+        if (gameOver) {
             return;
         }
-        console.log(count, deck_id);
         let config = {
             method: "get",
             url: "https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=" + count
         }
         axios(config)
             .then(res => {
+                console.log(hand);
                 setHand([...hand, ...res.data.cards])
                 setScore(calculateHandValue([...hand, ...res.data.cards]))
-                console.log(hand);
+                if (calculateHandValue([...hand, ...res.data.cards]) >= 21) {
+                    setGameOver(true);
+                }
             })
             .catch(err => {
                 console.log(err);
@@ -68,21 +70,30 @@ export const BlackJack = () => {
         // crate a horisontal card deck
         <div className="bg-slate-800 relative">
             <div className="container mx-auto h-screen bg-slate-800">
-                <div class="flex overflow-x-auto space-x-8 w-full snap-x snap-mandatory py-3">
+                <div className="flex overflow-x-auto space-x-8 w-full snap-x snap-mandatory py-3">
                     {hand.map((card, index) => {
-                        return <section key={index} class="flex-shrink-0 snap-center">
-                            <img src={card.image} alt={card.value} className="shadow w-screen px-4 sm:w-auto" />
+                        return <section key={index} className="flex-shrink-0 snap-center">
+                            <div className="px-4 w-screen sm:w-72 overflow-hidden">
+                                <img src={card.images["svg"]} alt={card.value} className="shadow scale-[3]" />
+                            </div>
+                            
+                            {/* <svg xmlns="http://www.w3.org/2000/svg">
+                                <image href={card.images["svg"]} width="100" height="100"
+                                />
+                            </svg> */}
                         </section>
                     })}
                 </div>
-                <h2 className={(score > 21 ? "text-red-500" : score > 17 ? "text-yellow-500" : score === 21 ? "text-green-500" : " text-slate-400") + (stand ? " text-6xl " : " ") + ("transition-all px-4")}>Score: {score}</h2>
+                <h2 className={(score > 21 ? "text-red-500" : score > 17 ? "text-yellow-500" : score === 21 ? "text-green-500" : " text-slate-400") + (gameOver ? " text-6xl " : " ") + ("transition-all px-4")}>Score: {score}</h2>
 
             </div>
             <div className="fixed bottom-0 left-0 w-full bg-white">
                 {/* create 2 buttons */}
                 <div className="flex justify-evenly p-4">
-                    {!stand && <Button1 onClick={() => { hit(1, deck.deck_id) }} color="#FF0000">Hit</Button1>}
-                    <Button1 onClick={() => { setStand(true) }} color="#00FF00">{stand ? "stood" : "stand"}</Button1>
+                    {!gameOver && <Button1 onClick={() => { hit(1, deck.deck_id) }} color="#FF0000">Hit</Button1>}
+                    <Button1 onClick={() => { setGameOver(true) }} color="#00ff00">
+                        {score === 21 ? "Blackjack!" : score > 21 ? "Bust" : gameOver ? "Game Over" : "Stand"}
+                    </Button1>
                 </div>
             </div>
         </div>
